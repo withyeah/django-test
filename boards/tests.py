ea495415d38251ceb4f0228acc3a4548aae0a031
withyeah <areygim@gmail.com>
2019-04-23 04:43:22 +0000

    03_index && delete && update view test

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,7 @@
 # from django.test import TestCase
 from test_plus.test import TestCase
 from django.conf import settings
+from django.urls import reverse
 from .models import Board
 from .forms import BoardForm
 
@@ -21,17 +22,13 @@ class BoardModelTest(TestCase):
         
     def test_02_test(self):
         # given
-        data = {
-            'title': '제목', 'content': '내용'
-        }
+        data = {'title': '제목', 'content': '내용'}
         # when then
         self.assertEqual(BoardForm(data).is_valid(), True)
         
     def test_03_boardform_without_title(self):
         # given
-        data = {
-            'content': '내용'
-        }
+        data = {'content': '내용'}
         # when then
         self.assertEqual(BoardForm(data).is_valid(), False)
         
@@ -44,7 +41,7 @@ class BoardViewTest(TestCase):
     # 테스트 시작할 때 깔고 들어갈 정보
     # 공통적인 given 상황을 구성하기에 유용하다.
     def setUp(self):
-        user = self.make_user(username='test', password='godqhrdl')
+        self.user = self.make_user(username='test', password='godqhrdl')
     
     # given, when, then 구조
     
@@ -80,4 +77,72 @@ class BoardViewTest(TestCase):
         # when
         with self.login(username='test', password='godqhrdl'):
             response = self.post('boards:create', data=data)
-            self.assertContains(response, '필')
+            self.assertContains(response, '필') 
+     
+    # detail 페이지가 제대로 출력되는지 확인         
+    def test_05_detail_contains(self):
+        # given
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        # when : 페이지가 뜹니까?
+        self.get_check_200('boards:detail', board_pk=board.pk)
+        # then : 내용을 불러옵니까? (더 집약적)
+        self.assertResponseContains(board.title, html=False)
+        self.assertResponseContains(board.content, html=False)
+        
+    def test_06_detail_template(self):
+        # given
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        # when
+        response = self.get_check_200('boards:detail', board_pk=board.pk)
+        # then : 너가 띄우는 템플릿이 이 이름이 맞아?
+        self.assertTemplateUsed(response, 'boards/detail.html')
+    
+    def test_07_get_index(self):
+        # 페이지가 잘 뜨는지 확인
+        # given when then
+        self.get_check_200('boards:index')
+        
+    def test_08_index_template(self):
+        # 페이지가 잘 뜨는지, 템플릿도 이름 맞는지 확인
+        # when then
+        response = self.get_check_200('boards:index')
+        self.assertTemplateUsed(response, 'boards/index.html')
+        
+    def test_09_index_queryset(self):
+        # given : 글 2개 작성
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        boards = Board.objects.order_by('-pk')
+        # when
+        response = self.get_check_200('boards:index')
+        # then
+        self.assertQuerysetEqual(response.context['boards'], map(repr, boards))
+    
+    # 에러 뜸! delete POST 로만 받게 돼있는데 get 으로 보내서    
+    def test_10_delete(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='godqhrdl'):
+            self.get_check_200('boards:delete', board_pk=board.pk)
+        
+    def test_11_dete_post(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='godqhrdl'):
+            self.post('boards:delete', board_pk=board.pk)
+            self.assertEqual(Board.objects.count(), 0)
+            
+    def test_12_delete_redirect(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='godqhrdl'):
+            response = self.post('boards:delete', board_pk=board.pk)
+            # then
+            self.assertRedirects(response, reverse('boards:index'))
+            
+    def test_13_get_update(self):
+        board = Board.objects.create(title='제목', content='내용', user=self.user)
+        with self.login(username='test', password='godqhrdl'):
+            response = self.get_check_200('boards:update', board.pk)
+            self.assertEqual(response.context['form'].instance.pk, board.pk)
+            
+    def test_14_get_update_login_required(self):
+        self.assertLoginRequired('boards:update', board_pk=1)
+        
