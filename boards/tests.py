1229daa1e36696936aa622329c0de61a4437d6d4
withyeah <areygim@gmail.com>
2019-04-23 01:48:23 +0000

    02_modelform test

--- boards/tests.py
+++ boards/tests.py
@@ -12,13 +12,33 @@ class SettingsTest(TestCase):
         self.assertEqual(settings.LANGUAGE_CODE, 'ko-kr')
         self.assertEqual(settings.TIME_ZONE, 'Asia/Seoul')
         
-# 2. Model test
+# 2. Model test + ModelForm test
 class BoardModelTest(TestCase):
     def test_01_model(self):
         # board = Board.objects.create(title='test title', content='test content')
         board = Board.objects.create(title='test title', content='test content', user_id=1)
         self.assertEqual(str(board), f'Board{board.pk}', msg='출력 값이 일치하지 않음')
         
+    def test_02_test(self):
+        # given
+        data = {
+            'title': '제목', 'content': '내용'
+        }
+        # when then
+        self.assertEqual(BoardForm(data).is_valid(), True)
+        
+    def test_03_boardform_without_title(self):
+        # given
+        data = {
+            'content': '내용'
+        }
+        # when then
+        self.assertEqual(BoardForm(data).is_valid(), False)
+        
+    def test_04_boardform_without_content(self):
+        data = {'title': '제목'}
+        self.assertEqual(BoardForm(data).is_valid(), False)
+    
 # 3. View test 
 class BoardViewTest(TestCase):
     # 테스트 시작할 때 깔고 들어갈 정보
@@ -60,4 +80,4 @@ class BoardViewTest(TestCase):
         # when
         with self.login(username='test', password='godqhrdl'):
             response = self.post('boards:create', data=data)
-            self.assertContains(response, '필수 항목입니다!')
+            self.assertContains(response, '필')
